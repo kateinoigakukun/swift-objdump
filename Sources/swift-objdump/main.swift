6e6b4d99f13d7d14ac76fd550b26f9a74d297740
Yuta Saito <kateinoigakukun@gmail.com>
2019-12-15 00:08:02 +0900

    up

--- Sources/swift-objdump/main.swift
+++ Sources/swift-objdump/main.swift
@@ -4,9 +4,7 @@ import MachOParser
 func main(arguments: [String]) {
     let filePath = arguments[1]
     let data = try! Data(contentsOf: URL(fileURLWithPath: filePath))
-    data.withUnsafeBytes { (ptr) in
-
-    }
+    SizeAnalyzer().report(object: data)
 }
 
 main(arguments: ProcessInfo.processInfo.arguments)
