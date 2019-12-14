c3472d5fe8dfe7425eb58f3bf101a7c8a249b5e6
Yuta Saito <kateinoigakukun@gmail.com>
2019-12-15 01:24:47 +0900

    wip

--- Sources/SwiftRuntime/RuntimeStructure.swift
+++ Sources/SwiftRuntime/RuntimeStructure.swift
@@ -1,32 +1,32 @@
-protocol RuntimeStruct {
+public protocol RuntimeStruct {
     static var size: Int { get }
     init(_: UnsafeRawPointer)
 }
 
-struct RuntimePair<E1: RuntimeStruct, E2: RuntimeStruct>: RuntimeStruct {
+public struct RuntimePair<E1: RuntimeStruct, E2: RuntimeStruct>: RuntimeStruct {
     private let ptr: UnsafeRawPointer
 
-    func project1() -> E1 { E1(ptr) }
-    func project2() -> E2 { E2(ptr.advanced(by: E1.size)) }
+    public func project1() -> E1 { E1(ptr) }
+    public func project2() -> E2 { E2(ptr.advanced(by: E1.size)) }
 
-    static var size: Int { E1.size + E2.size }
-    init(_ ptr: UnsafeRawPointer) { self.ptr = ptr }
+    public static var size: Int { E1.size + E2.size }
+    public init(_ ptr: UnsafeRawPointer) { self.ptr = ptr }
 }
 
-struct RuntimeUnion<E1: RuntimeStruct, E2: RuntimeStruct>: RuntimeStruct {
+public struct RuntimeUnion<E1: RuntimeStruct, E2: RuntimeStruct>: RuntimeStruct {
     private let ptr: UnsafeRawPointer
 
-    func project1() -> E1 { E1(ptr) }
-    func project2() -> E2 { E2(ptr) }
+    public func project1() -> E1 { E1(ptr) }
+    public func project2() -> E2 { E2(ptr) }
 
-    static var size: Int { max(E1.size, E2.size) }
-    init(_ ptr: UnsafeRawPointer) { self.ptr = ptr }
+    public static var size: Int { max(E1.size, E2.size) }
+    public init(_ ptr: UnsafeRawPointer) { self.ptr = ptr }
 }
 
-struct RuntimeValue<E>: RuntimeStruct {
-    static var size: Int { MemoryLayout<E>.size }
+public struct RuntimeValue<E>: RuntimeStruct {
+    public static var size: Int { MemoryLayout<E>.size }
     private let ptr: UnsafeRawPointer
 
-    func dereference() -> E { ptr.load(as: E.self) }
-    init(_ ptr: UnsafeRawPointer) { self.ptr = ptr }
+    public func dereference() -> E { ptr.load(as: E.self) }
+    public init(_ ptr: UnsafeRawPointer) { self.ptr = ptr }
 }
